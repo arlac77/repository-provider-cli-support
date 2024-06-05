1eb5e128c7186b10626aaac9d43f6ade2e1ce073
Markus Felten <Markus.Felten@gmx.de>
2024-06-05 14:39:09 +0100

    test: without .git url

--- tests/util-ava.mjs
+++ tests/util-ava.mjs
@@ -2,8 +2,9 @@ import test from "ava";
 import { repositoryUrl } from "repository-provider-cli-support";
 
 test("repositoryUrl", async t => {
-  t.is(
-    await repositoryUrl(import.meta.dirname + "/.."),
-    "https://github.com/arlac77/repository-provider-cli-support.git"
+  t.true(
+    (await repositoryUrl(import.meta.dirname + "/.."))?.startsWith(
+      "https://github.com/arlac77/repository-provider-cli-support"
+    )
   );
 });
